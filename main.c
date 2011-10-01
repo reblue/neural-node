ae4c75741e3b3de5672ccfd75456848b586ed2d2
rajan <the5bluebirds.com>
2011-10-01 11:36:07 +0530

    more testing

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include <stdlib.h>
 #include "percept.h"
+#include <stdio.h>
 
 int main(int argc, char **argv){
   Perceptron *p = create_perceptron(2,3);
   bin inputs[3] = {1,2,3};
   p_set_inputs(p, inputs);
+  p->grid[3]->memory = 2;
+  p->grid[3]->weights[0] = 1;  
+  p->grid[3]->weights[1] = 1;
+  p->grid[3]->weights[2] = 1;
   p_activate(p);
+  printf("%d\n", p->grid[3]->output);
   return EXIT_SUCCESS;
 }
